d14ff19d26a376aeeaec361f93b46507a5a5e8a9
Arunakumari B N <arunakumaribn@bmsit.in>
2021-10-26 19:07:41 +0530

    mistake with the previous commit, only user_table folder was added. User model and controller added now.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,8 @@
 class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def index
-    render plain: Todo.order(:due_date).map{|todo| todo.to_pleasant_string}.join("\n")
+    render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
   end
 
   def show
@@ -15,7 +16,7 @@ class TodosController < ApplicationController
     new_todo = Todo.create!(
       todo_text: todo_text,
       due_date: due_date,
-      completed: false
+      completed: false,
     )
     render plain: "New todo has been created with ID #{new_todo.id}"
   end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    render plain: "New user has been created with name #{new_user.name} and email #{new_user.email}"
+  end
+
+  def login
+    email = params[:email]
+    password = params[:password]
+    user = User.find_by(email: email, password: password)
+    render plain: user.present?
+  end
+end
