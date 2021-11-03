ad0b9594e3ea344d05d260042b59d17bb19edcc5
Arunakumari B N <arunakumaribn@bmsit.in>
2021-11-03 17:52:22 +0530

    Used check_box_tag, date_field_tag and text_field_tag. Also got rid of 'all' from todo.rb

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -5,18 +5,18 @@ class Todo < ActiveRecord::Base
   end
 
   def self.overdue
-    all.where("due_date < ? and completed = ?", Date.today, false)
+    where("due_date < ? and completed = ?", Date.today, false)
   end
 
   def self.due_today
-    all.where("due_date = ?", Date.today)
+    where("due_date = ?", Date.today)
   end
 
   def self.due_later
-    all.where("due_date > ?", Date.today)
+    where("due_date > ?", Date.today)
   end
 
   def self.completed
-    all.where(completed: true)
+    where(completed: true)
   end
 end
