596f670e8bfb3ddc1cca89543479f67d22bd57ed
Arunakumari B N <arunakumaribn@bmsit.in>
2021-11-03 07:57:38 +0530

    Delete functionality and authenticity token

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  # skip_before_action :verify_authenticity_token
 
   def index
     # render plain: Todo.order(:due_date).map { |todo| todo.to_pleasant_string }.join("\n")
@@ -28,6 +28,13 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save!
+    redirect_to todos_path
+  end
+
+  def destroy
+    id = params[:id]
+    todo = Todo.find(id)
+    todo.destroy
     redirect_to todos_path
   end
 end
