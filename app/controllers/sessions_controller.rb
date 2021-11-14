8a17732473d8484d805dad72eda279943b337656
Arunakumari B N <arunakumaribn@bmsit.in>
2021-11-14 16:37:41 +0530

    Render flash message of invalid login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
       session[:current_user_id] = user.id
       redirect_to "/"
     else
-      render plain: "incorrect"
+      flash[:error] = "Invalid login. Please retry."
+      redirect_to new_session_path
     end
   end
 
